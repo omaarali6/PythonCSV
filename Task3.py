e3d57f1a37ef299f1ed9a5d56f9ea0862bfa592a
Omar <omaaraly7@gmail.com>
2023-08-25 17:08:26 +0300

    Task3

---
+++ Task3.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+'Male and female counters'
+MaleCount = 0
+FemaleCount = 0
+
+
+'Declaring input and output files'
+file_name = "Employees.csv"
+file_name_output = "output.csv"
+
+'reading the csv file'
+df = pd.read_csv(file_name)
+
+'removing decimel points from the age column'
+df['Age'] = (df['Age']).astype(int)
+
+'converting salaries from USD to EGP (I hope by the time you read this the dollar would still be 30EGP hahaha)'
+df['Salary(USD)'] = (df['Salary(USD)']*30)
+
+'remove any duplicate rows'
+df.drop_duplicates(subset=None, inplace=True)
+
+'extracting the gender coloumn'
+x = df['Gender']
+
+
+'looping on the gender column to extract how many males and females are there '
+for i in x:
+	if i == "M":
+		MaleCount = MaleCount + 1
+	if i == "F":
+		FemaleCount = FemaleCount + 1
+
+
+'printing male/female ratio'
+print("the relation between the males and females is " + str(MaleCount/FemaleCount))
+
+'printing the madian salaries'
+print("Median Salaries is "+ str(df['Salary(USD)'].mean()))
+
+'printing the average age'
+print("Average Age is "+ str(df['Age'].mean()))
+df.to_csv(file_name_output, index=False)
